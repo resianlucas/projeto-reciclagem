fc0ed292be08f127add3ba4a44dbc57a1417001c
resianlucas <101301367+resianlucas@users.noreply.github.com>
2024-11-17 23:13:59 -0300

    Delete projeto-reciclagem directory

--- projeto-reciclagem/src/main/java/br/com/reciclagem/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.reciclagem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.reciclagem.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-
-}
